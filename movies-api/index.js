76a256a30ddec1be81069e75fd457191235d9883
jgarrone82 <jgarrone@logytechmobile.com>
2021-11-23 09:46:44 -0300

    First Version

---
+++ movies-api/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+
+const { config } = require('./config/index');
+const moviesApi = require('./routes/movies');
+
+const {
+  logErrors,
+  wrapErrors,
+  errorHandler
+} = require('./utils/middleware/errorHandlers.js');
+
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
+
+// body parser
+app.use(express.json());
+
+// routes
+moviesApi(app);
+
+// Catch 404
+app.use(notFoundHandler);
+
+// Errors middleware
+app.use(logErrors);
+app.use(wrapErrors);
+app.use(errorHandler);
+
+app.get('/', function (req, res) {
+  res.send('hello world');
+});
+
+app.get('/json', function (req, res) {
+  res.json({ hello: 'world' });
+});
+
+app.listen(config.port, function () {
+  console.log(`Listening http://localhost:${config.port}`);
+});
